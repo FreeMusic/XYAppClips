73a5898a3c0183f2dd19639f3f410ac43f44169e
孙晓阳 <958589819@qq.com>
2020-07-24 15:03:25 +0800

    Initial Commit

---
+++ XYSwiftUIApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  XYSwiftUIApp
+//
+//  Created by 乐逍遥 on 2020/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
